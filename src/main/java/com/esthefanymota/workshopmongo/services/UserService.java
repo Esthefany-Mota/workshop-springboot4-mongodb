9a2d0e2e483936be9cf27e06c2b1f65f771df2d1
Esthefany Mota <mottavitoria0601@gmail.com>
2026-01-16 19:21:45 -0300

    Obtendo um usuário por id

--- src/main/java/com/esthefanymota/workshopmongo/services/UserService.java
+++ src/main/java/com/esthefanymota/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.esthefanymota.workshopmongo.services;
 
 import com.esthefanymota.workshopmongo.domain.User;
 import com.esthefanymota.workshopmongo.repository.UserRepository;
+import com.esthefanymota.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,5 +21,14 @@ public class UserService {
 
     public User save(User user){
         return repo.save(user);
+    }
+
+    public User findById(String id){
+        Optional<User> user = repo.findById(id);
+        if(user.isEmpty()){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+
+        return user.orElse(null);
     }
 }
