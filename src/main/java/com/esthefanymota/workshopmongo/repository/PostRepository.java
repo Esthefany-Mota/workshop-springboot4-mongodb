9775cff8108aa6000ee9b6d033182bb9705a8821
Esthefany Mota <mottavitoria0601@gmail.com>
2026-01-19 17:07:00 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/esthefanymota/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.esthefanymota.workshopmongo.repository;
+
+import com.esthefanymota.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
